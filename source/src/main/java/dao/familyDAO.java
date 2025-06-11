73f9e36fc1021cce11959bdc46fdb5219b07f94a
plusdojo2025-osaki <osaki-hikaru-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-11 14:49:47 +0900

    eclipseの初期設定

--- source/src/main/java/dao/familyDAO.java
+++ source/src/main/java/dao/familyDAO.java
@@ -2,9 +2,5 @@ package dao;
 
 public class familyDAO {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
 
 }
--- source/src/main/java/dto/user.java
+++ source/src/main/java/dto/user.java
@@ -2,9 +2,4 @@ package dto;
 
 public class user {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-	}
-
 }
