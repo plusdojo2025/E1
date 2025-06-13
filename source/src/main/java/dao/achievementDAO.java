9eba6e9aac1955a9b3b0c9f69b267abb3ab944ee
plusdojo2025-Ijichi <ijichi-miyu-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 15:31:16 +0900

    achievementDAO一旦完成

--- source/src/main/java/dao/achievementDAO.java
+++ source/src/main/java/dao/achievementDAO.java
@@ -1,67 +1,3 @@
-/*
-package dao;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class achievementDAO {
-	
-	public int select(String user_id, String date, int achieve_history) {
-		Connection conn = null;
-		int daily_count = 0;
-		
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// データベースに接続する
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/e1_db?"
-				+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
-			
-			// SQL文を準備する
-			String sql = "SELECT user_id, achievement_history "
-					+ "FROM achievement "
-					+ "WHERE date = CURDATE() - INTERVAL 1 DAY";
-			
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
-			// SQL文を実行し、結果表を取得する
-			ResultSet rs = pStmt.executeQuery();
-			while (rs.next()) {
-				String userId = rs.getString("user_id");
-				int history = rs.getInt("achievement_history");
-				result.put(userId, history);
-				}
-
-			
-						
-		}catch (SQLException e) {
-			e.printStackTrace();
-			daily_count = 0;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			daily_count = 0;
-		} finally {
-			// データベースを切断
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-					daily_count = 0;
-				}
-			}
-		}
-		
-		
-		return daily_count;
-	}
-	
-}*/
 package dao;
 
 import java.sql.Connection;
@@ -77,7 +13,7 @@ import dto.achievement;
 public class achievementDAO {
 
     // 昨日の achievement データを全件取得
-    public List<achievement> selectYesterdayAchievement() {
+    public List<achievement> selectYesterdayAchievement(String family_id) {
         Connection conn = null;
         List<achievement> yesterdayList = new ArrayList<>();
 
@@ -91,9 +27,10 @@ public class achievementDAO {
                 "root", "password");
 
             // SQL文を準備する
-            String sql = "SELECT achieve_id, user_id, date, achievement_history "
-            			+ "FROM achievement "
-            			+ "WHERE date = CURDATE() - INTERVAL 1 DAY";
+            String sql = "SELECT user_id, SUM(achieve_history) AS daily_score "
+            		+ "FROM achievement "
+            		+ "WHERE date = CURDATE() - INTERVAL 1 DAY AND family_id = " + family_id 
+            		+ "GROUP BY user_id";
             
             PreparedStatement pStmt = conn.prepareStatement(sql);
             
@@ -101,12 +38,10 @@ public class achievementDAO {
             ResultSet rs = pStmt.executeQuery();
 
             while (rs.next()) {
-                int id = rs.getInt("achieve_id");
                 String user_id = rs.getString("user_id");
-                String date = rs.getString("date");
-                int history = rs.getInt("achievement_history");
+                int daily_score = rs.getInt("daily_score");
 
-                achievement a = new achievement(id, user_id, date, history);
+                achievement a = new achievement(0, user_id, null, daily_score);
                 yesterdayList.add(a);
             }
 
@@ -123,6 +58,54 @@ public class achievementDAO {
 
         // 結果を返す
         return yesterdayList;
+    }
+    
+    // 過去12か月分の achievement データを全件取得
+    public List<achievement> selectYearAchievement(String family_id) {
+        Connection conn = null;
+        List<achievement> yearList = new ArrayList<>();
+
+        try {
+        	// JDBCドライバを読み込む
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // データベースに接続する
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/e1_db?characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9",
+                "root", "password");
+
+            // SQL文を準備する
+            String sql = "SELECT user_id, COUNT(*) AS monthly_score "
+            		+ "FROM achievement "
+            		+ "WHERE date >= CURDATE() - INTERVAL 12 MONTH AND family_id = " + family_id
+            		+ "GROUP BY user_id";
+            
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            
+            // SQL文を実行し、結果表を取得する
+            ResultSet rs = pStmt.executeQuery();
+
+            while (rs.next()) {
+                String user_id = rs.getString("user_id");
+                int monthly_score = rs.getInt("monthly_score");
+
+                achievement a = new achievement(0, user_id, null, monthly_score);
+                yearList.add(a);
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+            	// データベースを切断
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 結果を返す
+        return yearList;
     }
 }
 
