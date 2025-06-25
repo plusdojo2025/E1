6415f36dd735d2582048a2e8400a948df6b97c69
plusdojo2025-osaki <osaki-hikaru-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-25 13:30:04 +0900

    ホーム非同期

--- source/src/main/java/servlet/TestServlet.java
+++ source/src/main/java/servlet/TestServlet.java
@@ -2,19 +2,19 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
+
+import dao.today_houseworkDAO;
 
 /**
  * Servlet implementation class TestServlet
@@ -28,51 +28,31 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		JSONObject json = new JSONObject();
-		String noti_housework = "";
-		int id = Integer.parseInt(request.getParameter("housework_id"));
-		
-		try {
-			// JDBCドライバを読み込む
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// データベースに接続する
-			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/e1?"
-			+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-			"root", "password");
-			
-			String sql = "SELECT housework_name FROM housework WHERE housework_id = " + id + "";
-			
-			PreparedStatement pStmt = conn.prepareStatement(sql);
-			ResultSet rs = pStmt.executeQuery();
-			
-			 while (rs.next()) {
-				 noti_housework = rs.getString("housework_name");		
-			}
-			 json.put("data", noti_housework);
-			 
-			// データベースを切断
-			if (conn != null) {
-				try {
-				conn.close();
-				} catch (SQLException e) {
-				e.printStackTrace();
-				}
-			}
-		}catch (SQLException e) {
-			e.printStackTrace();
-			} 
-			catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			}
-		
-		//httpヘッダー送信の登録
-		response.setContentType("application/json");
-		response.setHeader("Cache-Control", "nocache");
-		response.setCharacterEncoding("utf-8");
-	    //送信データの登録
-		PrintWriter out = response.getWriter();
-		//送信データをネットストリームへ書き込む
-		out.print(json);
+				JSONObject json = new JSONObject();
+				HttpSession session = request.getSession();
+				LocalDateTime nowDate = LocalDateTime.now();
+				DateTimeFormatter dtf1 =
+						DateTimeFormatter.ofPattern("yyyy-MM-dd");
+							String formatNowDate1 = dtf1.format(nowDate);
+				 
+				String user_id = (String) session.getAttribute("user_id");
+				String family_id = (String) session.getAttribute("family_id"); 
+				int housework_id = Integer.parseInt(request.getParameter("housework_id"));
+				today_houseworkDAO td_hwDAO = new today_houseworkDAO();
+				
+				if(td_hwDAO.selectachive2(formatNowDate1, housework_id) == -1) {
+					td_hwDAO.submit(user_id, family_id ,housework_id);
+					td_hwDAO.insert_notification(user_id,housework_id);
+					int idList = td_hwDAO.selectachive2(formatNowDate1, housework_id);
+					json.put("data", idList);
+					//httpヘッダー送信の登録
+					response.setContentType("application/json");
+					response.setHeader("Cache-Control", "nocache");
+					response.setCharacterEncoding("utf-8");
+					//送信データの登録
+					PrintWriter out = response.getWriter();
+					//送信データをネットストリームへ書き込む
+					out.print(json);
+					}
 	}
 }
